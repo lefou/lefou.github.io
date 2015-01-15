726a5b67e6bad5308a35c378fc9b5c5e1b5ae022
Tobias Roeser <le.petit.fou@web.de>
2015-01-15 10:41:55 +0100

    Added git staging to buildscript

--- SBuild.scala
+++ SBuild.scala
@@ -5,6 +5,8 @@ import de.tototec.sbuild._
 @classpath("/home/lefou/work/tototec/sbuild/jbake/org.sbuild.plugins.jbake/target/org.sbuild.plugins.jbake-0.1.2.9000.jar")
 class SBuild(implicit _project: Project) {
 
+  val repo = "https://github.com/lefou/lefou.github.io.git"
+  val branch = "master"
   Target("phony:clean").evictCache exec {
     Path("target").deleteRecursive
   }
@@ -15,5 +17,31 @@ class SBuild(implicit _project: Project) {
     targetDir = Path("target"),
     jbakeVersion = JBakeVersion.Packaged("2.3.2")
    ) }
+
+  Target("phony:prepare-target") exec {
+    val dir = Path("target")
+    dir.mkdirs()
+    import sys.process._
+    val exitVal = Process(
+      Seq("git", "clone", "--depth", "1", "--single-branch", "--branch", branch, repo, dir.getPath),
+      cwd = dir
+      ) #&&
+      Process(Seq("git", "rm", "-rf", "."), cwd = dir) !;
+    if (exitVal != 0) throw new RuntimeException("Exit code: " + exitVal)
+  }
+
+  Target("phony:stage-site-to-git") dependsOn "clean" ~~ "prepare-target" ~~ "jbake" exec {
+    val dir = Path("target")
+    import sys.process._
+    val code = Process(Seq("git", "add", "*"), cwd = dir) !;
+    if(code != 0) throw new RuntimeException("Exit code: " + code)
+  }
+
+  Target("phony:build-and-commit-site") dependsOn "stage-site-to-git" exec {
+    val dir = Path("target")
+    import sys.process._
+    val code = Process(Seq("git", "commit"), cwd = dir) !<;
+    if(code != 0) throw new RuntimeException("Exit code: " + code)
+  }
 
 }
